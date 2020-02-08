1d0669c26c403cea4629919929330436c66c5947
Tim Winters <twinters@wpi.edu>
2020-02-08 02:36:15 -0500

    Initial Commit

---
+++ src/main/java/edu/wpi/teamname/App.java
@@ -0,0 +1,32 @@
+package edu.wpi.teamname;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+  private static Stage primaryStage;
+
+  public static Stage getPrimaryStage() {
+    return primaryStage;
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    App.primaryStage = primaryStage;
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("fxml/Scene1.fxml"));
+      Scene scene = new Scene(root);
+      primaryStage.setScene(scene);
+      primaryStage.show();
+    } catch (IOException e) {
+      e.printStackTrace();
+      Platform.exit();
+    }
+  }
+}
